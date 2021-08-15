06e08d752cbc60a43e3b2c9749f224b50d343fb7
Shannon Whitley <swhitley@whitleymedia.com>
2021-08-14 19:25:48 -0700

    Minor Updates
    - Icon added
    - MetaNames on parameters.

--- CommandLineOptions.cs
+++ CommandLineOptions.cs
@@ -9,22 +9,22 @@ namespace CLARiNET
 {
     public class Options
     {
-        [Value(index: 0,Required = false, HelpText = "Full path and file name for the CLAR")]
+        [Value(index: 0,MetaName = "CLAR File", Required = false, HelpText = "Full path and file name for the CLAR")]
         public string ClarFile { get; set; }
 
-        [Value(index: 1, Required = false, HelpText = "Cloud Collection name")]
+        [Value(index: 1, MetaName = "Collection", Required = false, HelpText = "Cloud Collection name")]
         public string CollectionName { get; set; }
 
-        [Value(index: 2, Required = false, HelpText = "Number associated with a Workday environment")]
+        [Value(index: 2, MetaName = "Environment Number", Required = false, HelpText = "Number associated with a Workday environment (list all with -e parameter)")]
         public string EnvNum { get; set; }
 
-        [Value(index: 3, Required = false, HelpText = "Workday Tenant")]
+        [Value(index: 3, MetaName = "Tenant", Required = false, HelpText = "Workday Tenant")]
         public string Tenant { get; set; }
 
-        [Value(index: 4, Required = false, HelpText = "Username")]
+        [Value(index: 4, MetaName = "Username", Required = false, HelpText = "Username")]
         public string Username { get; set; }
 
-        [Value(index: 5, Required = false, HelpText = "Password")]
+        [Value(index: 5, MetaName = "Password", Required = false, HelpText = "Password")]
         public string Password { get; set; }
 
         [Option('e', "environments", Required = false,
